fa8dfb6c459731f726c278b216936e912e85b24d
Yousef Dawood <yousefdawood31@gmail.com>
2025-10-03 19:16:38 +0300

    setup t3.env and prisma with neon's postgresql

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
+import { notFound } from "next/navigation";
 import Placeholder from "@/components/Placeholder";
-import { Button } from "@/components/ui/button";
 import { tickets } from "@/data";
 import TicketItem from "@/features/ticket/components/TicketItem";
 
@@ -12,19 +12,9 @@ export default async function page({ params }: PageProps) {
   const { id } = await params;
   const activeTicket = tickets.find((ticket) => ticket.id === id);
 
+  // prettier-ignore
   if (!activeTicket)
-    return (
-      <Placeholder
-        message="Ticket not found"
-        button={
-          <Button variant={"outline"}>
-            <Link href="/tickets" className="font-semibold">
-              Go to Tickets
-            </Link>
-          </Button>
-        }
-      />
-    );
+    notFound()
 
   return (
     <section className="mx-auto w-full max-w-xl">
