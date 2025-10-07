8e45ddbd4ee57557bc2635407c39ee65e66d992c
Yousef Dawood <yousefdawood31@gmail.com>
2025-10-07 05:28:07 +0300

    add delete tickets using ORM

--- src/features/ticket/actions/actions.ts
+++ src/features/ticket/actions/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 
 export async function deleteTicket(id: string) {
   await prisma.tickets.delete({ where: { id } });
+
+  revalidatePath("/tickets");
   return redirect("/tickets");
 }
