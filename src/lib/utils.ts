de0a79161e41e6c7f2f6440a7f2f03e582755844
Yousef Dawood <yousefdawood31@gmail.com>
2025-12-05 14:20:35 +0200

    add tryCatch utility function for improved error handling in async operations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type Success<T> = {
+  data: T;
+  error: null;
+};
+
+type Fail<F = Error> = {
+  data: null;
+  error: F;
+};
+
+export async function tryCatch<T, F = Error>(
+  promise: () => Promise<T> | Promise<T>,
+  onThrow?: boolean,
+): Promise<Success<T> | Fail<F>> {
+  try {
+    const data = await promise?.();
+
+    return { data, error: null };
+  } catch (error) {
+    console.log("ERROR => ", error);
+    if (onThrow) throw error;
+    return { data: null, error: error as F };
+  }
+}
